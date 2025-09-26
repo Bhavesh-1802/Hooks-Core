d5410b3255136bf1b89c40c1d6b8440c56cead3e
Bhavesh <bhanumehta26@gmail.com>
2025-09-26 13:43:07 +0530

    Initial Spring Boot setup for hooks-backend

---
+++ src/main/java/com/hooks/hooks_core/HooksCoreApplication.java
@@ -0,0 +1,13 @@
+package com.hooks.hooks_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HooksCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HooksCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hooks/hooks_core/HooksCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hooks.hooks_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HooksCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
